f03e95e23d533fe879b6a8ccdff42cdaf3aed2b3
Trần Đình Nam <namtd@aht-asia.com>
2024-01-05 15:13:37 +0700

    add project folder structure

---
+++ MyIOSApplication/MyIOSApplication/AppDelegates/MyIOSApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyIOSApplicationApp.swift
+//  MyIOSApplication
+//
+//  Created by AHT on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyIOSApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
